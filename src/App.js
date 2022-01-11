13cc5e6be2329ad32b35e26a6e551591a3299904
tianjin <tianjin@doctorwork.com>
2022-01-11 16:00:02 +0800

    feat: fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import { useState, useEffect } from 'react'
+import request from 'axios';
+import { Layout, Menu, Breadcrumb, List, Typography  } from 'antd';
 import './App.css';
-import { Button } from 'antd';
-import 'antd/dist/antd.css';
+const { Header, Content, Footer } = Layout;
 function App() {
+  const [list, setList] = useState();
+
+  useEffect(() => {
+    request.get('http://127.0.0.1:3000/get').then((res) => {
+      setList(res.data)
+      console.log(res.data)
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      <Button>按钮</Button>
-    </div>
+    <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+        <List
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={list}
+          renderItem={item => (
+            <List.Item>
+              <Typography.Text mark>[ITEM]</Typography.Text> {item.title}
+            </List.Item>
+          )}
+        />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
 }
 

bb19350eaf2c8c16f3e087d048e4f43ccf916d28
tianjin <tianjin@doctorwork.com>
2022-02-16 16:58:50 +0800

    refactor: 优化

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,16 @@
-import { get, put } from '../request'
+import { get, put } from '../request';
+
 export const getList = async (params: {selection?: string, isFile?: Boolean, isPublish?: Boolean}) =>{
-  const res = await get('http://127.0.0.1:3000/get', { params });
+  const res = await get('/get', { params });
   return res.data
 }
 
 export const getArticle = async (params: { id: string, conType: string }) => {
-  const res = await get('http://127.0.0.1:3000/get_article', {params});
+  const res = await get('/get_article', {params});
   return res.data
 }
 
 export const increaseViews = async (body: { id: string }) => {
-  const res = await put('http://127.0.0.1:3000/increase_views', body);
+  const res = await put('/increase_views', body);
   return res.data
 }
--- src/request/index.ts
+++ src/request/index.ts
@@ -1,20 +1,20 @@
   
 import request, { AxiosRequestConfig } from 'axios';
-
+const domain = 'http://127.0.0.1:3000'
 
 export const get = async (url: string, config?: AxiosRequestConfig):Promise<any> => {
-   const res = await request.get(url, config);
+   const res = await request.get(domain + url, config);
    return res.data;
 }
 
 export const post = async (url: string, data?: any, config?: AxiosRequestConfig):Promise<any> => {
-   const res = await request.post(url, data, config);
+   const res = await request.post(domain + url, data, config);
    return res.data;
    
 }
 
 
 export const put = async  (url: string, data:any, config?: AxiosRequestConfig):Promise<any> => {
-  const res = await request.put(url, data, config);
+  const res = await request.put(domain + url, data, config);
   return res.data;
 }
